5f7edaf1b5097fd150ded710d25c4e310becde25
Natalie Taktachev <ntaktach@gmail.com>
2019-07-22 04:15:57 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(num)
+  if num < 0 || num == 0 || num == 1
+    false
+  else
+    n = 2
+    while n < num
+      return false if num % n == 0
+      n += 1
+    end
+    true
+  end
+end
